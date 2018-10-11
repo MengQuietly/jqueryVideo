6e1fa5ad07f2257b18322aa525e1311432b45d94
mengmeng <wenjing@hiqianbao.com>
2018-10-11 10:33:26 +0800

    04-scrollTop&&scrollLeft：top滚动距离（scrollTop()）、left滚动距离（scrollLeft()）

--- 第三天/01-特殊属性操作/03-width&height方法/width_height_func.js
+++ 第三天/01-特殊属性操作/03-width&height方法/width_height_func.js
@@ -28,8 +28,4 @@ $(document).ready(function () {
     //     console.log($(window).height);
     // });
 
-
-
-
-
 });
---
+++ 第三天/01-特殊属性操作/04-scrollTop&&scrollLeft方法/scrolltop_scrollleft_func.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function () {
+
+    $(window).scroll(function () {
+        // 1）top滚动距离；
+        console.log($(window).scrollTop());
+        // 2）left滚动距离；
+        console.log($(window).scrollLeft());
+    });
+
+});
