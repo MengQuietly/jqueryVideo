6c47b6ab2916a62674e99e2f34eb4f9fac37009c
mengmeng <wenjing@hiqianbao.com>
2018-10-12 16:25:25 +0800

    18-²突：jquery.js文件放后面，使用$.noConflict() 释放控制权；

---
+++ 第三天/18-$冲突/conflict.js
@@ -0,0 +1,28 @@
+
+$(document).ready(function () {
+
+    // other.js 文件中的 $ 与 jquery.js 文件中的 $ 发生冲突；
+
+    // 解决办法：
+    // 1）jquery.js 文件 写于 other.js 文件 后；
+    // 2.1）使用 $.noConflict(); // jQuery.js 释放$的控制权;
+    // 3.1) 使用 jQuery(func) 重新定义；
+    // 2.2) 或 使用 var $$ = $.noConflict(); // 定义；
+    // 3.2）使用 $$(func) 重新定义；
+    console.log($);
+
+    // jQuery释放$的控制权
+    $.noConflict();
+    console.log($);
+    jQuery(function () {
+        console.log($);
+    });
+
+    // var $$ = $.noConflict();
+    console.log($);
+    // $$(function () {
+    //     console.log($);
+    // });
+
+
+});
