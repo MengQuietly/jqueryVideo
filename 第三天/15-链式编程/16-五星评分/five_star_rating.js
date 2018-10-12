14b8e1b72957d8689f41f6c3bf4d902b5bef9f25
mengmeng <wenjing@hiqianbao.com>
2018-10-12 15:53:21 +0800

    16-五星好评：end() 回到上一次操作；

--- 第三天/15-链式编程/16-五星评分/five_star_rating.js
+++ 第三天/15-链式编程/16-五星评分/five_star_rating.js
@@ -6,9 +6,13 @@ $(document).ready(function () {
 
     //1. 给li注册鼠标经过事件，让自己和前面所有的兄弟都变成实心
     $(".comment>li").on("mouseenter", function () {
-        // prevAll() 获得当前匹配元素集合中每个元素的前面的同胞元素
+        // prevAll() 获得当前匹配元素集合中前面的同胞元素；
+        // nextAll() 获得当前元素集合中后面的同胞元素；
        $(this).text(star_s).prevAll().text(star_s);
         $(this).nextAll().text(star_k);
+
+        // end() 回到上一次操作；
+        // $(this).text(star_s).prevAll().text(star_s).end().nextAll().text(star_k);
     });
 
     //2. 给ul注册鼠标离开时间，让所有的li都变成空心
