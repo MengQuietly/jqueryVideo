3af2c1ca81cd5ddf7d28f52412bfaed4661f71a5
mengmeng <wenjing@hiqianbao.com>
2018-10-12 16:05:51 +0800

    17-each方法：each(func(index,element)); index当前下标，element当前元素；

---
+++ 第三天/17-each方法/each_func.js
@@ -0,0 +1,13 @@
+
+$(document).ready(function () {
+
+    // 使用 for 循环
+    // for (var i = 0; i < $("li").length; i++) {
+    //     $("li").eq(i).css("opacity", (i+1)/10);
+    // }
+
+    // 使用 each 方法；element：当前元素，index：当前下标；
+    $("li").each(function (index, element) {
+       $(element).css("opacity", (index+1)/10);
+    });
+});
